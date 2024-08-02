747037acf843f3d3311653d86699da2f37aba913
Alexandr Prokopovich <prokop7@mail.ru>
2024-08-02 17:12:55 +0300

    Migrate project to Next.js

--- src/__tests__/MainPage.test.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { render } from '@testing-library/react';
-import MainPage from '../components/MainPage';
-
-test('renders MainPage component', () => {
-  const { container } = render(<MainPage searchTerm="" />);
-  expect(container.firstChild).toHaveClass('main-page');
-});
--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-// src/components/Footer.tsx
-import React from 'react';
-import styles from './Footer.module.css';
-
-const Footer: React.FC = () => {
-  return <footer className={styles.footer}></footer>;
-};
-
-export default Footer;
